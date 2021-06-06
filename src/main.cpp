6e4ec6986c2e16e0ecc57bf53f6610af8dcc0e97
adrifern48 <adrifern48@gmail.com>
2021-06-06 21:08:13 +0000

    introduce opencv

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
-int main() {
-    std::cout << "Hello World!" << "\n";
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cout << "usage: ./VehicleDetector <path/to/image>\n";
+        return -1;
+    }
+
+    cv::Mat img;
+    img = cv::imread(argv[1], 1);
+
+    if (!img.data) {
+        std::cout << "Invalid image data!\n";
+        return -1;
+    }
+
+    cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Display Image", img);
+
+    cv::waitKey(0);
+
     return 0;
 }
